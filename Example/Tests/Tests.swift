2fc1ce45b509d82d00346a0d3831910971df39dd
Yuuki Nishiyama <tetujin@ht.sfc.keio.ac.jp>
2018-12-07 02:15:04 +0200

    Add test code and fix minor issues

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,11 +1,13 @@
 import XCTest
+import RealmSwift
 import com_awareframework_ios_sensor_timezone
 
 class Tests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
     }
     
     override func tearDown() {
@@ -13,16 +15,129 @@ class Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testSync(){
+        //        let sensor = TimezoneSensor.init(TimezoneSensor.Config().apply{ config in
+        //            config.debug = true
+        //            config.dbType = .REALM
+        //        })
+        //        sensor.start();
+        //        sensor.enable();
+        //        sensor.sync(force: true)
+        
+        //        let syncManager = DbSyncManager.Builder()
+        //            .setBatteryOnly(false)
+        //            .setWifiOnly(false)
+        //            .setSyncInterval(1)
+        //            .build()
+        //
+        //        syncManager.start()
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+    func testStorage(){
+        let sensor = TimezoneSensor.init(TimezoneSensor.Config().apply{ config in
+            config.dbType = .REALM
+        })
+        let timezoneStorageExpect = expectation(description: "timezone storage")
+        let observer = NotificationCenter.default.addObserver(forName: Notification.Name.actionAwareTimezone,
+                                                              object: nil,
+                                                              queue: .main) { (notification) in
+            if let engine = sensor.dbEngine {
+                if let data = engine.fetch(TimezoneData.TABLE_NAME, TimezoneData.self, nil) as? Results<Object> {
+                    print(data.count)
+                    if data.count != 1{
+                        XCTFail()
+                    }else{
+                        timezoneStorageExpect.fulfill()
+                    }
+                }
+            }
         }
+        sensor.start()
+        wait(for: [timezoneStorageExpect], timeout: 10)
+        sensor.stop()
+        NotificationCenter.default.removeObserver(observer)
+    }
+    
+    func testObserver(){
+        
+        class Observer:TimezoneObserver{
+            weak var timezoneExpectation: XCTestExpectation?
+            func onTimezoneChanged(data: TimezoneData) {
+                self.timezoneExpectation?.fulfill()
+            }
+        }
+        
+        let timezoneObserverExpect = expectation(description: "Timezone observer")
+        let observer = Observer()
+        observer.timezoneExpectation = timezoneObserverExpect
+        let sensor = TimezoneSensor.init(TimezoneSensor.Config().apply{ config in
+            config.sensorObserver = observer
+        })
+        sensor.start()
+        wait(for: [timezoneObserverExpect], timeout: 3)
+        sensor.stop()
+    }
+    
+    func testControllers(){
+        
+        let sensor = TimezoneSensor()
+        
+        /// test set label action ///
+        let expectSetLabel = expectation(description: "set label")
+        let newLabel = "hello"
+        let labelObserver = NotificationCenter.default.addObserver(forName: .actionAwareTimezoneSetLabel, object: nil, queue: .main) { (notification) in
+            let dict = notification.userInfo;
+            if let d = dict as? Dictionary<String,String>{
+                XCTAssertEqual(d[TimezoneSensor.EXTRA_LABEL], newLabel)
+            }else{
+                XCTFail()
+            }
+            expectSetLabel.fulfill()
+        }
+        sensor.set(label:newLabel)
+        wait(for: [expectSetLabel], timeout: 5)
+        NotificationCenter.default.removeObserver(labelObserver)
+        
+        /// test sync action ////
+        let expectSync = expectation(description: "sync")
+        let syncObserver = NotificationCenter.default.addObserver(forName: Notification.Name.actionAwareTimezoneSync , object: nil, queue: .main) { (notification) in
+            expectSync.fulfill()
+            print("sync")
+        }
+        sensor.sync()
+        wait(for: [expectSync], timeout: 5)
+        NotificationCenter.default.removeObserver(syncObserver)
+        
+        
+        //// test start action ////
+        let expectStart = expectation(description: "start")
+        let observer = NotificationCenter.default.addObserver(forName: .actionAwareTimezoneStart,
+                                                              object: nil,
+                                                              queue: .main) { (notification) in
+                                                                expectStart.fulfill()
+                                                                print("start")
+        }
+        sensor.start()
+        wait(for: [expectStart], timeout: 5)
+        NotificationCenter.default.removeObserver(observer)
+        
+        
+        /// test stop action ////
+        let expectStop = expectation(description: "stop")
+        let stopObserver = NotificationCenter.default.addObserver(forName: .actionAwareTimezoneStop, object: nil, queue: .main) { (notification) in
+            expectStop.fulfill()
+            print("stop")
+        }
+        sensor.stop()
+        wait(for: [expectStop], timeout: 5)
+        NotificationCenter.default.removeObserver(stopObserver)
+    }
+    
+    func testTimezoneData(){
+        let data = TimezoneData()
+        let dict = data.toDictionary()
+        
+        XCTAssertEqual(dict["timezoneId"] as? String, "")
     }
     
 }
--- com.awareframework.ios.sensor.timezone/Classes/TimezoneSensor.swift
+++ com.awareframework.ios.sensor.timezone/Classes/TimezoneSensor.swift
@@ -14,7 +14,7 @@ extension Notification.Name{
     public static let actionAwareTimezoneStart = Notification.Name(TimezoneSensor.ACTION_AWARE_TIMEZONE_START)
     public static let actionAwareTimezoneStop = Notification.Name(TimezoneSensor.ACTION_AWARE_TIMEZONE_STOP)
     public static let actionAwareTimezoneSync = Notification.Name(TimezoneSensor.ACTION_AWARE_TIMEZONE_SYNC)
-    public static let actionAwareTImezoneSetLabel = Notification.Name(TimezoneSensor.ACTION_AWARE_TIMEZONE_SET_LABEL)
+    public static let actionAwareTimezoneSetLabel = Notification.Name(TimezoneSensor.ACTION_AWARE_TIMEZONE_SET_LABEL)
 }
 
 public protocol TimezoneObserver {
@@ -28,29 +28,29 @@ public class TimezoneSensor: AwareSensor {
     /**
      * Broadcast event: when there is new timezone information
      */
-    public static let ACTION_AWARE_TIMEZONE = "ACTION_AWARE_TIMEZONE"
+    public static let ACTION_AWARE_TIMEZONE = "com.awareframework.ios.sensor.timezone"
     public static let EXTRA_DATA = "data"
     
     /**
      * Received event: Fire it to start the timezone sensor.
      */
-    public static let ACTION_AWARE_TIMEZONE_START = "com.aware.sensor.timezone.SENSOR_START"
+    public static let ACTION_AWARE_TIMEZONE_START = "com.awareframework.ios.sensor.timezone.SENSOR_START"
     
     /**
      * Received event: Fire it to stop the timezone sensor.
      */
-    public static let ACTION_AWARE_TIMEZONE_STOP = "com.aware.sensor.timezone.SENSOR_STOP"
+    public static let ACTION_AWARE_TIMEZONE_STOP = "com.awareframework.ios.sensor.timezone.SENSOR_STOP"
     
     /**
      * Received event: Fire it to sync the data with the server.
      */
-    public static let ACTION_AWARE_TIMEZONE_SYNC = "com.aware.sensor.timezone.SYNC"
+    public static let ACTION_AWARE_TIMEZONE_SYNC = "com.awareframework.ios.sensor.timezone.SYNC"
     
     /**
      * Received event: Fire it to set the data label.
      * Use [EXTRA_LABEL] to send the label string.
      */
-    public static let ACTION_AWARE_TIMEZONE_SET_LABEL = "com.aware.sensor.timezone.SET_LABEL"
+    public static let ACTION_AWARE_TIMEZONE_SET_LABEL = "com.awareframework.ios.sensor.timezone.SET_LABEL"
     
     /**
      * Label string sent in the intent extra.
@@ -82,6 +82,10 @@ public class TimezoneSensor: AwareSensor {
         
     }
     
+    public override convenience init() {
+        self.init(Config())
+    }
+    
     public init(_ config:TimezoneSensor.Config){
         super.init()
         CONFIG = config
@@ -96,16 +100,16 @@ public class TimezoneSensor: AwareSensor {
                 self.retrieveTimezone()
             })
             self.timer?.fire()
+            self.notificationCenter.post(name:.actionAwareTimezoneStart, object:nil)
         }
-        self.notificationCenter.post(name:.actionAwareTimezoneStart, object:nil)
     }
     
     public override func stop() {
         if let t = self.timer{
             t.invalidate()
             self.timer = nil
+            self.notificationCenter.post(name:.actionAwareTimezoneStop, object:nil)
         }
-        self.notificationCenter.post(name:.actionAwareTimezoneStop, object:nil)
     }
     
     public override func sync(force: Bool = false) {
@@ -113,11 +117,11 @@ public class TimezoneSensor: AwareSensor {
             engine.startSync(TimezoneData.TABLE_NAME, TimezoneData.self, DbSyncConfig.init().apply{config in
                 config.debug = self.CONFIG.debug
             })
+            self.notificationCenter.post(name:.actionAwareTimezoneSync, object:nil)
         }
-        self.notificationCenter.post(name:.actionAwareTimezoneSync, object:nil)
     }
     
-    func retrieveTimezone(){
+    public func retrieveTimezone(){
         let currentTimezone = TimeZone.current.identifier
         print(currentTimezone)
         if self.lastTimezone == "" {
@@ -132,7 +136,7 @@ public class TimezoneSensor: AwareSensor {
         self.lastTimezone = currentTimezone
     }
     
-    func saveTimezoneData(_ timezoneId:String) {
+    public func saveTimezoneData(_ timezoneId:String) {
         
         let tzData = TimezoneData()
         tzData.timezoneId = timezoneId
@@ -145,6 +149,15 @@ public class TimezoneSensor: AwareSensor {
             observer.onTimezoneChanged(data: tzData)
         }
         
-        self.notificationCenter.post(name: .actionAwareTimezone, object: nil, userInfo: [TimezoneSensor.EXTRA_DATA:tzData])
+        self.notificationCenter.post(name: .actionAwareTimezone,
+                                     object: nil,
+                                     userInfo: [TimezoneSensor.EXTRA_DATA:tzData])
+    }
+    
+    public func set(label:String){
+        self.CONFIG.label = label
+        self.notificationCenter.post(name: .actionAwareTimezoneSetLabel,
+                                     object: nil,
+                                     userInfo:[TimezoneSensor.EXTRA_LABEL:label])
     }
 }
